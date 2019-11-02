01cab8b23f8a4201fe7af4da6611be77c98c37d1
Rishabh Raizada <rishabh5102000@gmail.com>
2019-11-02 18:07:26 +0530

    added location functionality

--- lib/providers/great_places.dart
+++ lib/providers/great_places.dart
@@ -24,7 +24,7 @@ class GreatPlaces with ChangeNotifier {
     );
     _items.add(newPlace);
     notifyListeners();
-    DBHelper.insert('places', {
+    DBHelper.insert('user_places', {
       'id': newPlace.id,
       'title': newPlace.title,
       'image': newPlace.image.path,
@@ -32,14 +32,16 @@ class GreatPlaces with ChangeNotifier {
   }
 
   Future<void> fetchAndSetPlaces() async {
-    final dataList = await DBHelper.getData('places');
+    final dataList = await DBHelper.getData('user_places');
     _items = dataList
-        .map((item) => Place(
-              id: item['id'],
-              title: item['title'],
-              location: null,
-              image: File(item['image']),
-            ))
+        .map(
+          (item) => Place(
+                id: item['id'],
+                title: item['title'],
+                image: File(item['image']),
+                location: null,
+              ),
+        )
         .toList();
     notifyListeners();
   }
